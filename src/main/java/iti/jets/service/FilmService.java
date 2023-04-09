2fc26c5f7e6134d42415d8920aa91c5c108f4a50
NeuroBoy <91805629+a7medRo7oma@users.noreply.github.com>
2023-04-09 23:05:50 +0200

    remove useless files

---
+++ src/main/java/iti/jets/service/FilmService.java
@@ -0,0 +1,24 @@
+package iti.jets.service;
+
+import java.util.Set;
+
+import iti.jets.model.Film;
+
+
+public class FilmService {
+
+    public static void updateFilm(Film film) {
+    }
+
+    public static Film getFilmbyID(int id) {
+        return null;
+    }
+
+    public static void addFilm(Film film) {
+    }
+
+    public static Set<Film> getAllFilms() {
+        return null;
+    }
+    
+}
